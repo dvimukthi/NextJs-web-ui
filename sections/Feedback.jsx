38e3ae4507c0f99dba5e79db674b3a5168f2c055
dvimukthi <deshanvimukthi59@gmail.com>
2023-03-02 18:50:38 +0530

    feedback text

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -14,11 +14,23 @@ const Feedback = () => (
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
     >
       <motion.div
-      variants={fadeIn('right', 'tween' 0.2, 1)}
-      className="flex-[0.5] lg:max-w-[37px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
       >
         <div className="feedback-gradient" />
-        <div></div>
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+            Mike
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+            Founder | Metaversus
+          </p>
+        </div>
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[22px] leading-[39px] text-white">
+          "With the development of today's technology, Metaverse is very useful
+          for today's work, or can be called web 3.0 by using metaverse you can
+          use is as anything"
+        </p>
       </motion.div>
     </motion.div>
   </section>
