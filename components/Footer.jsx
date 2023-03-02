d311c9deb7fca20c434bc3e7f11f4bb96363e9f6
dvimukthi <deshanvimukthi59@gmail.com>
2023-03-02 20:29:12 +0530

    footer copyright section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -21,7 +21,25 @@ const Footer = () => (
         <button
           type="button"
           className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]"
-        ></button>
+        >
+          <img
+            src="/headset.svg"
+            alt="headset"
+            className="w-[24px] h-[24px] object-contain"
+          />
+          <span className="font-normal text-[16px] text-white">
+            ENTER METAVERSE
+          </span>
+        </button>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4 className="font-extrabold text-[24px] text-white">METAVERSE</h4>
+          <p className="font-normal text-[14px] text-white opacity-50">
+            Copyright 2023 Metaversus. All rights reserved.
+          </p>
+        </div>
       </div>
     </div>
   </motion.footer>
